d478d59cf1e08deb604ae8761b31cdb535c2478d
Beggar13 <gmapetz@gmail.com>
2021-10-30 10:54:33 +0100

    Initial object-solution branch commit

--- training6.js
+++ training6.js
@@ -8,7 +8,7 @@ function deleteNth(arr, n) {
   //   const timesArr = Object.values(emptyArr);
 
   let newArr = [];
-  let count = new Array(arr.length - 1).fill(0);
+  let count = new Array(arr.length).fill(0);
   console.log(count);
   for (i = 0; i < arr.length; i++) {
     if (newArr.includes(arr[i])) {
@@ -19,14 +19,13 @@ function deleteNth(arr, n) {
     } else {
       newArr.push(arr[i]);
       count[i]++;
-      // TODO fix the counting
     }
   }
   console.log(newArr);
 }
 
-deleteNth([1, 1, 1, 1], 2);
 deleteNth([20, 37, 20, 21, 37, 50, 21, 25, 21, 20], 2);
+deleteNth([1, 1, 1, 1], 2);
 
 // function countEl(arr) {
 //   let emptyArr = {};
